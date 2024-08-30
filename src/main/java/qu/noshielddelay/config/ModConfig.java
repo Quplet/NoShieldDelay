b3fa12202b400097d4aac289a5f278cb3a2d407d
Quplet <jonathanhart1114@gmail.com>
2024-08-29 21:21:13 -0400

    Improved logging

--- src/main/java/qu/noshielddelay/config/ModConfig.java
+++ src/main/java/qu/noshielddelay/config/ModConfig.java
@@ -30,13 +30,14 @@ public class ModConfig {
         spec.defineInList("Enabled", true, Arrays.asList(true, false));
         spec.defineInRange("RaiseTime", 1, 0, 5);
         if (!spec.isCorrect(config)) {
-            NoShieldDelay.LOGGER.error("One or more config settings were incorrect, setting to default value(s)");
-            config.setComment("Enabled", "This is a Fabric recreation of Revvilo's Responsive Shield's mod. Their mod is exclusively for Forge and I wanted a Fabric one for myself, so here you go.\n" +
-                    "Minecraft by default has a hard-coded 5 tick delay between right-clicking with the shield and being able to block.\n" +
-                    "Setting RaiseTime to 0 makes shields capable of blocking instantly. Setting it to 5 would be equivalent to vanilla.");
+            NoShieldDelay.LOGGER.warn("One or more config settings were incorrect, setting to default value(s)");
+            config.setComment("Enabled", """
+                    This is a Fabric recreation of Revvilo's Responsive Shield's mod. Their mod is exclusively for Forge and I wanted a Fabric one for myself, so here you go.
+                    Minecraft by default has a hard-coded 5 tick delay between right-clicking with the shield and being able to block.
+                    Setting RaiseTime to 0 makes shields capable of blocking instantly. Setting it to 5 would be equivalent to vanilla.""");
             ConfigSpec.CorrectionListener listener = (action, path, incorrectValue, correctedValue) -> {
                 String pathString = String.join(",", path);
-                NoShieldDelay.LOGGER.error("Corrected {}: was {}, is now {}", pathString, incorrectValue, correctedValue);
+                NoShieldDelay.LOGGER.info("Corrected {}: was {}, is now {}", pathString, incorrectValue, correctedValue);
             };
             spec.correct(config, listener);
             config.save();
